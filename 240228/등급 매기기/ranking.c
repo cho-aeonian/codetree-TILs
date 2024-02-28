46c9026cf133ae194c5a542cbfec2ab31163e43d
초영 <choxaeonian@gmail.com>
2024-02-28 18:09:37 +0900

    Title: 등급 매기기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/등급 매기기/ranking.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num>=90) {
+        printf("A");
+    }
+    else if(num<=89 && num>=80) {
+        printf("B");
+    }
+    else if(num<=79 && num>=70) {
+        printf("C");
+    }
+    else if (num<=69 && num>=60) {
+        printf("D");
+    }
+    else {
+        printf("F");
+    }
+    return 0;
+}
