1af99a020c5b55f9f4d4970b29c33fea1851f9bf
초영 <choxaeonian@gmail.com>
2024-03-02 03:01:04 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/숫자의 계절은/season-of-num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int m;
+    scanf("%d",&m);
+    if(m>=12 || m<=2) {
+        printf("Winter");
+    }
+    else if(m<=5) {
+        printf("Spring");
+    }
+    else if (m<=8) {
+        printf("Summer");
+    }
+    else {
+        printf("Fall");
+    }
+    return 0;
+}
